ecee276aa37e577167360022368dc5fcf8ac58bc
Rio Gwyneth Soliva <20170012516@my.xu.edu.ph>
2022-09-21 12:44:37 -1200

    Create HomePage

---
+++ deesgourmet/src/pages/HomePage.jsx
@@ -0,0 +1,11 @@
+export default function HomePage() {
+  return (
+    <div>
+      <div className="flex flex-col items-center">
+        <h1 className="font-bold text-xl">Party Menu</h1>
+        <p>The most commonly order food from Dee’s Gourmet</p>
+      </div>
+
+    </div>
+  );
+}
