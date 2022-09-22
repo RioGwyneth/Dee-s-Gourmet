3d4a09eb586fc77f6d0888a13e9c70564eb9fb62
Rio Gwyneth Soliva <20170012516@my.xu.edu.ph>
2022-09-21 16:24:35 -1200

    Create card category component

---
+++ deesgourmet/src/components/Button.jsx
@@ -0,0 +1,18 @@
+export default function Button({
+  text = "",
+  padding = "p-2",
+  bgcolor,
+  width,
+  textcolor = "text-white",
+  custom,
+  onClick,
+}) {
+  return (
+    <button
+      className={`${padding} ${width} ${bgcolor} ${textcolor} ${custom} flex items-center justify-center gap-2 font-medium `}
+      onClick={onClick}
+    >
+      {text}{" "}
+    </button>
+  );
+}
